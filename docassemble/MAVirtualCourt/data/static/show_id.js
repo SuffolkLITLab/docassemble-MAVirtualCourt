0e9bc25f392403ffd05362a65399c7289c0729c3
System Administrator <qsteenhuis@gmail.com>
2020-06-28 15:40:46 -0400

    Add ids, saved answer

---
+++ docassemble/MAVirtualCourt/data/static/show_id.js
@@ -0,0 +1,3 @@
+$(document).on('daPageLoad', function(){
+  $("#qId").html("<code>Page id: " + daQuestionID['id'] + "</code>");
+});
---
+++ docassemble/MAVirtualCourt/icon.py
@@ -0,0 +1,12 @@
+def fa_icon(icon, color="primary", color_css=None, size="sm"):
+  """
+  Return HTML for a font-awesome icon of the specified size and color. You can reference
+  a CSS variable (such as Bootstrap theme color) or a true CSS color reference, such as 'blue' or 
+  '#DDDDDD'. Defaults to Bootstrap theme color "primary".
+  """
+  if not color and not color_css:
+    return ':' + icon + ':' # Default to letting Docassemble handle it
+  elif color_css:
+    return '<i class="fa fa-' + icon + ' fa-' + size + '" style="color:' + color_css + ';"></i>'
+  else:
+    return '<i class="fa fa-' + icon + ' fa-' + size + '" style="color:var(--' + color + ');"></i>'
