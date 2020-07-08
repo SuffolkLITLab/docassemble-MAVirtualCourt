4cf2d4fdbdcd7530f8fb6f8249b3730e9cb35f5d
Anonymous User <52798256+plocket@users.noreply.github.com>
2020-07-08 08:58:09 -0400

    Add ability to show package version number in dev info/hud

--- docassemble/MAVirtualCourt/data/static/show_id.js
+++ docassemble/MAVirtualCourt/data/static/show_id.js
@@ -1,3 +1,11 @@
 $(document).on('daPageLoad', function(){
-  $("#qId").html("<code>Page id: " + daQuestionID['id'] + "</code>");
+  // I believe this is async. We won't rely on `await` for now -
+  // it's less browser compatible.
+  get_interview_variables( function( data ){
+    var verText = ''
+    if ( data.success && data.variables.package_version_number ) {
+      verText = ', v' + data.variables.package_version_number;
+    }
+    $( "#qId" ).html( "<code>Page id: " + daQuestionID['id'] + verText + "</code>" );
+  });
 });
