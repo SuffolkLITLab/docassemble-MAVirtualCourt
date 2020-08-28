79eed3e379be9d35bdbec7c9f64727c6f330a699
plocket <52798256+plocket@users.noreply.github.com>
2020-08-28 17:47:52 -0400

    Fix no wildcard

--- tests/puppeteer-utils.js
+++ tests/puppeteer-utils.js
@@ -12,7 +12,8 @@ const initPuppeteer = async () => {
   const browser = await puppeteer.launch({headless: !process.env.DEBUG});
   const page = await browser.newPage();
   page.setDefaultTimeout(120 * 1000);
-
+  await page.screenshot({ path: `error-foo.jpg`, type: 'jpeg' });
+  browser.close()
   return {'page': page, 'browser': browser};
 }
 
