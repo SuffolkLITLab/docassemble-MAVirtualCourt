5e84af2dcd7919c653b53e44a50169b187dc50e6
plocket <52798256+plocket@users.noreply.github.com>
2020-08-31 17:34:11 -0400

    Let local developer develop more easily

--- tests/interview-constants.js
+++ tests/interview-constants.js
@@ -2,7 +2,8 @@ const putils = require('./puppeteer-utils');
 
 // Each repo has its own interview that it's testing
 // We'll deal with multiple urls when it becomes an issue
-const INTERVIEW_URL = `${putils.BASE_INTERVIEW_URL}%3A209a_package.yml`;
+let filename = '209a_package';  // Easier to edit
+const INTERVIEW_URL = process.env.INTERVIEW_URL || `${putils.BASE_INTERVIEW_URL}%3A${filename}.yml`;
 
 
 module.exports = {
