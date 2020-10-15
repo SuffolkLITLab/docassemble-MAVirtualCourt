74feaf2b6b8c6146d5bd697ce769673810db14a9
System Administrator <qsteenhuis@gmail.com>
2020-10-15 19:38:32 -0400

    Support new Appeals Court in MACourts

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.MAVirtualCourt',
-      version='1.0.13',
+      version='1.0.14',
       description=(''),
       long_description='# docassemble.MAVirtualCourt\r\n\r\n\r\n\r\n## Author\r\n\r\nQuinten Steenhuis\r\nCaroline Robinson\r\nKate Barry\r\nPlocket\r\nLily Yang\r\nMatthew Brooks\r\nLance Godard\r\nMaeve MacGlinchey\r\nKendall Garner\r\nDavid Colarusso',
       long_description_content_type='text/markdown',
