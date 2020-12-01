a4c7ca87bb29a379fc23e5abd275a066eda148cf
System Administrator <qsteenhuis@gmail.com>
2020-12-01 17:39:20 -0500

    Add dependency on new feedback form package

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(name='docassemble.MAVirtualCourt',
       url='https://docassemble.org',
       packages=find_packages(),
       namespace_packages=['docassemble'],
-      install_requires=['docassemble.AssemblyLineToolbox', 'docassemble.MACourts>=0.0.47', 'docassemble.assemblylinewizard>=0.39'],
+      install_requires=['docassemble.AssemblyLineToolbox', 'docassemble.GithubFeedbackForm>=0.0.1', 'docassemble.MACourts>=0.0.47', 'docassemble.assemblylinewizard>=0.39'],
       zip_safe=False,
       package_data=find_package_data(where='docassemble/MAVirtualCourt/', package='docassemble.MAVirtualCourt'),
      )
